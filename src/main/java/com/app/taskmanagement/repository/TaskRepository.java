7ea6b3095b37d86daf91d21601adc12c19d41137
peyman Azish <azishsam74@gmail.com>
2026-08-31 15:33:17 +0330

    chore: enable Hibernate SQL logging for N+1 analysis

--- src/main/java/com/app/taskmanagement/repository/TaskRepository.java
+++ src/main/java/com/app/taskmanagement/repository/TaskRepository.java
@@ -6,6 +6,7 @@ import com.app.taskmanagement.model.enums.Priority;
 import com.app.taskmanagement.model.enums.TaskStatus;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,7 +14,8 @@ public interface TaskRepository extends JpaRepository<Task,Long> {
     List<Task> findTaskByCreatedBy(User createdBy);
 
     List<Task> findTaskByAssignedTo(User assignedTo);
-
+    @Query(""" 
+        select t from Task t join fetch t.createdBy join fetch t.assignedTo where t.priority = :priority""")
     List<Task> findTaskByPriority(Priority priority);
 
     List<Task> findTaskByStatusAndAssignedTo(TaskStatus status, User assignedTo);
